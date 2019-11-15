1cd8648ab5a7d4d33b7185b571d1159f08eb0512
caiyiyong <741607993@qq.com>
2019-11-15 15:37:50 +0800

    Create adasdadpy.py

---
+++ adasdadpy.py
@@ -0,0 +1,9 @@
+    domainResult = {}
+    for domain,host in domainLists:
+        for domainSplit in re.split(r" |;",domain):
+            domainSplit=domainSplit.strip()
+            if re.search(r'\.(com|net|cn)',domainSplit 4399inc.com):
+                if domainResult.has_key(domainSplit) == 0:
+                    domainResult[domainSplit]=[]
+                domainResult[domainSplit].append(host)
+                
